7e327ae3d460f2b226f6d26014252cf05feb95d6
ralphfromthesky <93632926+ralphfromthesky@users.noreply.github.com>
2023-08-18 14:59:05 +0800

    new

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-const toggleClose = () => {
-    
-}
+const logins = document.getElementById('logins');
+const registers = document.getElementById('registers')
+const logInBtn = document.getElementById('logInBtn');
+let isVsisble = false;
+const show = () => {
+  registers.classList.toggle('fade-element');
+  logins.classList.toggle('fade-in');
+  isVsisble = !isVsisble;
+};
+
+logInBtn.addEventListener('click', () => {
+    if (logInBtn.textContent == 'Log In') {
+        logInBtn.textContent = 'Register';
+    } else {
+        logInBtn.textContent = 'Log In';
+    }
+    show();
+})
+
+
+// toggleButtons.forEach((toggleButton) => {
+//   toggleButton.addEventListener('click', slide);
+// });
