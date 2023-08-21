84718d5c2364a16617fb88463e11cdd158497f18
ralphfromthesky <93632926+ralphfromthesky@users.noreply.github.com>
2023-08-21 17:14:11 +0800

    modify

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const logins = document.getElementById('logins');
 const registers = document.getElementById('registers')
 const logInBtn = document.getElementById('logInBtn');
-let isVsisble = false;
 const show = () => {
   registers.classList.toggle('fade-element');
   logins.classList.toggle('fade-in');
-  isVsisble = !isVsisble;
 };
 
 logInBtn.addEventListener('click', () => {
@@ -17,7 +15,16 @@ logInBtn.addEventListener('click', () => {
     show();
 })
 
+const bottomImage = document.querySelector('.arrow');
+window.addEventListener('scroll', () => {
+    const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrolledDistance = window.scrollY;
 
-// toggleButtons.forEach((toggleButton) => {
-//   toggleButton.addEventListener('click', slide);
-// });
+    // Adjust the value below to determine how close to the bottom the user needs to be to show the image
+    const triggerDistance = 100;
+    if (scrolledDistance > scrollableHeight - triggerDistance) {
+        bottomImage.style.opacity = '1';
+    } else {
+        bottomImage.style.opacity = '0';
+    }
+});
